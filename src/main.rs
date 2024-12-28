4f384fcde6b754d310a40d47e4b57be6057df167
Your Name <you@example.com>
2024-12-27 23:42:20 -0800

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::{self, Write};
 use std::process;
 
 fn main() {
+    let path_env = std::env::var("PATH").unwrap();
     // Uncomment this block to pass the first stage
     loop {
         print!("$ ");
@@ -17,11 +18,13 @@ fn main() {
         } else if let Some(code) = trimmed.strip_prefix("exit ") {
             process::exit(code.parse::<i32>().expect("Not a number"));
         } else if let Some(command) = trimmed.strip_prefix("type ") {
-            match command {
-                "echo" => println!("echo is a shell builtin"),
-                "exit" => println!("exit is a shell builtin"),
-                "type" => println!("type is a shell builtin"),
-                _ => println!("{}: not found", command),
+            let mut paths = path_env.split(":");
+            if let Some(found) =
+                paths.find(|path| std::fs::metadata(format!("{path}/{command}")).is_ok())
+            {
+                println!("{command} is in {found}/{command}")
+            } else {
+                println!("{command}: not found")
             }
         } else {
             println!("{}: command not found", trimmed);

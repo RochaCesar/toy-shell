97f3e15d271a7cd26cea8eec71ce277271f58bd8
Your Name <you@example.com>
2024-12-27 22:41:40 -0800

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
     // Uncomment this block to pass the first stage
@@ -9,12 +10,20 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        match input.as_str().trim() {
-            "exit 0" => break,
-            trimmed => match trimmed.strip_prefix("echo ") {
-                Some(rest) => println!("{rest}"),
+        let trimmed = input.as_str().trim();
+
+        if let Some(rest) = trimmed.strip_prefix("echo ") {
+            println!("{rest}");
+        } else if let Some(code) = trimmed.strip_prefix("exit ") {
+            process::exit(code.parse::<i32>().expect("Not a number"));
+        } else if let Some(command) = trimmed.strip_prefix("type ") {
+            match command {
+                "echo" => println!("echo is a shell builtin"),
+                "exit" => println!("exit is a shell builtin"),
                 _ => println!("{}: command not found", trimmed),
-            },
+            }
+        } else {
+            println!("{}: command not found", trimmed);
         }
     }
 }
